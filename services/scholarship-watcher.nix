3ceb47126523b6905cab5892408ebcb2f87df77d
Giovanni <indirizzoemail@mail.com>
2021-07-10 17:26:24 +0200

    remove useless config for scholarship-watcher service

--- services/scholarship-watcher.nix
+++ services/scholarship-watcher.nix
@@ -4,9 +4,6 @@
   systemd.services.scholarship-watcher = {
     enable = true;
     description = "Watch for scholarship updates";
-    unitConfig = {
-      Type = "simple";
-    };
     serviceConfig = {
       ExecStart = "/home/giovanni/scholarship-watcher/target/release/scholarship-watcher";
     };
