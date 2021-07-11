ef5928f63102f74c33c34397dd2f3c92203b28b5
Giovanni <indirizzoemail@mail.com>
2021-07-11 19:50:42 +0200

    allow barrier port

--- hosts/swan/configuration.nix
+++ hosts/swan/configuration.nix
@@ -102,6 +102,10 @@
     enableSSHSupport = true;
   };
 
+  networking.firewall.allowedTCPPorts = [ 24800 ];
+  # networking.firewall.allowedUDPPorts = [ ... ];
+  networking.firewall.enable = true;
+
   # This value determines the NixOS release from which the default
   # settings for stateful data, like file locations and database versions
   # on your system were taken. It‘s perfectly fine and recommended to leave
