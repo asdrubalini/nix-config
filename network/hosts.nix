ca7ac1e33c09db22120e6e09336ab14b12598def
Giovanni <indirizzoemail@mail.com>
2021-07-12 12:38:03 +0200

    update ips

--- network/hosts.nix
+++ network/hosts.nix
@@ -2,8 +2,8 @@
   networking.extraHosts =
     ''
       168.119.171.204	arrow
-      10.0.0.16		swan
+      10.0.0.150	swan
       10.0.0.2		pearl
-      10.0.0.215	looking-glass
+      10.0.0.16		looking-glass
     '';
 }
