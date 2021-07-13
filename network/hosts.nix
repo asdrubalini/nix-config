70765031b47885a824f18f3dc0e289bd42e6169d
Giovanni <indirizzoemail@mail.com>
2021-07-13 18:27:51 +0200

    update hosts to add orchid ip

--- network/hosts.nix
+++ network/hosts.nix
@@ -2,8 +2,9 @@
   networking.extraHosts =
     ''
       168.119.171.204	arrow
-      10.0.0.150	swan
-      10.0.0.2		pearl
-      10.0.0.16		looking-glass
+      10.0.0.150	    swan
+      10.0.0.2		    pearl
+      10.0.0.16		    looking-glass
+      157.90.171.48	    orchid
     '';
 }
