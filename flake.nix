beee50bfc0e654be553218c55458e3241bb74f9d
asdrubalini <asdrubalini@mail.com>
2022-04-07 02:13:46 +0200

    lock

--- flake.nix
+++ flake.nix
@@ -2,8 +2,10 @@
   description = "NixOS configurations for Asdrubalini";
 
   inputs = {
-    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
-    home-manager.url = "github:nix-community/home-manager";
+    nixpkgs.url =
+      "github:nixos/nixpkgs/bc4b9eef3ce3d5a90d8693e8367c9cbfc9fc1e13";
+    home-manager.url =
+      "github:nix-community/home-manager/07b941f0c45ac4af6732d96f4cb6142824eee3df";
     home-manager.inputs.nixpkgs.follows = "nixpkgs";
   };
 
