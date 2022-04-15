3a55fd488f1590c34abf5fda862cde2f94a3f232
asdrubalini <asdrubalini@mail.com>
2022-04-15 15:28:19 +0200

    add nix-index

--- hosts/swan.nix
+++ hosts/swan.nix
@@ -125,9 +125,6 @@ in {
 
   time.timeZone = "Europe/Rome";
 
-  # The global useDHCP flag is deprecated, therefore explicitly set to false here.
-  # Per-interface useDHCP will be mandatory in the future, so this generated config
-  # replicates the default behaviour.
   networking.useDHCP = false;
   networking.interfaces.eno1.useDHCP = true;
   networking.interfaces.wlp4s0.useDHCP = true;
@@ -168,6 +165,7 @@ in {
     libnotify
     neovim
     git
+    nix-index
 
     systemApply
   ];
