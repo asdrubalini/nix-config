4c989c14014877135a40421fff5f06e50fb1b77c
asdrubalini <asdrubalini@mail.com>
2022-05-04 20:36:06 +0200

    emacs

--- desktop/emacs/init.el
+++ desktop/emacs/init.el
@@ -148,6 +148,31 @@
   :config
   (put 'dired-find-alternate-file 'disabled nil))
 
+(use-package dired-sidebar
+  :straight t
+  :ensure t
+  :bind (("C-x C-n" . dired-sidebar-toggle-sidebar))
+  :commands (dired-sidebar-toggle-sidebar)
+  :init
+  (add-hook 'dired-sidebar-mode-hook
+            (lambda ()
+              (unless (file-remote-p default-directory)
+                (auto-revert-mode))))
+  :config
+  (push 'toggle-window-split dired-sidebar-toggle-hidden-commands)
+  (push 'rotate-windows dired-sidebar-toggle-hidden-commands)
+
+  (setq dired-sidebar-subtree-line-prefix "__")
+  (setq dired-sidebar-theme 'vscode)
+  (setq dired-sidebar-use-term-integration t)
+  (setq dired-sidebar-use-custom-font t))
+
+(defun sidebar-toggle ()
+  "Toggle both `dired-sidebar' and `ibuffer-sidebar'."
+  (interactive)
+  (dired-sidebar-toggle-sidebar)
+  (ibuffer-sidebar-toggle-sidebar))
+
 (use-package helm
   :straight t
   :ensure t
