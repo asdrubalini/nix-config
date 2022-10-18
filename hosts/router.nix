2aa4e0f7a0879e3bf57cae98b88606eeb7cf9d28
Asdrubalini <asdrubalini@mail.com>
2022-10-19 01:36:39 +0200

    router

---
+++ hosts/router.nix
@@ -0,0 +1,104 @@
+{ config, pkgs, lib, modulesPath, ... }:
+
+{
+  imports = [
+    (modulesPath + "/profiles/qemu-guest.nix")
+    ../services/ssh-secure.nix
+  ];
+
+  boot.initrd.availableKernelModules = [ "uhci_hcd" "ehci_pci" "ahci" "virtio_pci" "sr_mod" "virtio_blk" ];
+  boot.initrd.kernelModules = [ ];
+  boot.kernelModules = [ ];
+  boot.extraModulePackages = [ ];
+
+  fileSystems."/" =
+    { device = "/dev/disk/by-uuid/a00746d8-02d2-4b0b-8226-3745c33918c8";
+      fsType = "btrfs";
+      options = [ "subvol=@" ];
+    };
+
+  fileSystems."/home" =
+    { device = "/dev/disk/by-uuid/a00746d8-02d2-4b0b-8226-3745c33918c8";
+      fsType = "btrfs";
+      options = [ "subvol=@home" ];
+    };
+
+  fileSystems."/nix" =
+    { device = "/dev/disk/by-uuid/a00746d8-02d2-4b0b-8226-3745c33918c8";
+      fsType = "btrfs";
+      options = [ "subvol=@nix" ];
+    };
+
+  fileSystems."/boot" =
+    { device = "/dev/disk/by-uuid/6CAD-375A";
+      fsType = "vfat";
+    };
+
+  swapDevices = [ ];
+
+  networking = {
+    interfaces.enp1s0f0.useDHCP = lib.mkDefault true;
+
+    interfaces.enp1s0f1.ipv4.addresses = [ {
+      address = "10.0.0.1";
+      prefixLength = 20;
+    } ];
+
+    defaultGateway = "10.0.0.200";
+    nameservers = ["10.0.0.3"];
+
+    interfaces.enp6s18.useDHCP = lib.mkDefault true;
+  };
+
+
+  hardware.cpu.intel.updateMicrocode = lib.mkDefault config.hardware.enableRedistributableFirmware;
+
+  nixpkgs.config.allowUnfree = true;
+  networking.hostName = "router";
+
+  i18n.defaultLocale = "en_US.UTF-8";
+  console = {
+    earlySetup = true;
+    font = "${pkgs.terminus_font}/share/consolefonts/ter-132n.psf.gz";
+    packages = with pkgs; [ terminus_font ];
+  };
+
+  users = {
+    mutableUsers = false;
+    extraUsers.root.hashedPassword = (import ../passwords).password;
+
+    users.giovanni = {
+      isNormalUser = true;
+      extraGroups = [ "wheel" ];
+      hashedPassword = (import ../passwords).password;
+    };
+  };
+
+  environment.systemPackages = with pkgs; [ git ];
+
+  programs.neovim.enable = true;
+  programs.neovim.viAlias = true;
+
+  services.iperf3 = {
+    enable = true;
+    openFirewall = true;
+    verbose = true;
+  };
+
+  nix = {
+    # package = pkgs.nixFlakes;
+    extraOptions = ''
+      experimental-features = nix-command flakes
+    '';
+  };
+
+  users.users."giovanni".openssh.authorizedKeys.keys =
+    [
+      (import ../ssh-keys/the-hydra.nix).key
+    ];
+  
+  # networking.firewall.allowedTCPPorts = [ ... ];
+  # networking.firewall.allowedUDPPorts = [ ... ];
+
+  system.stateVersion = "22.05";
+}
