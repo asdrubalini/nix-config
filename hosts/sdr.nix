36ad0a16b260f059760edd43ca3fd767d4dffa28
Asdrubalini <asdrubalini@mail.com>
2022-11-19 16:19:51 +0100

    sdr

--- hosts/sdr.nix
+++ hosts/sdr.nix
@@ -38,6 +38,7 @@
   # Use the systemd-boot EFI boot loader.
   boot.loader.systemd-boot.enable = true;
   boot.loader.efi.canTouchEfiVariables = true;
+  boot.loader.efi.efiSysMountPoint = "/boot/efi";
 
   nixpkgs.config.allowUnfree = true;
   networking.hostName = "sdr";
